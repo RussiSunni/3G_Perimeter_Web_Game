06175e7c6bf63ffca9041ac8eb34c64d5f2df425
RussiSunni <jonathandyason@hotmail.com>
2022-10-12 11:44:59 +0200

    asd

--- js/introScene.js
+++ js/introScene.js
@@ -4,16 +4,21 @@ class IntroScene extends Phaser.Scene {
     }
     preload() {
         //load our images or sounds 
-
+        this.load.image("shark01", "images/shark01.png");
     }
     create() {
         //define our objects
-        console.log("Ready!");
+        // The text.
         this.text1 = this.add.text(game.config.width / 2, game.config.height / 2 - 200, "Interactive Lessons", { fontFamily: "Arial", color: "#ffd966", fontSize: "50px" });
         this.text1.setOrigin(0.5, 0.5);
 
         this.text2 = this.add.text(game.config.width / 2, game.config.height / 2 - 100, "PERIMETER", { fontFamily: "Arial", fontSize: "100px" });
         this.text2.setOrigin(0.5, 0.5);
+
+        // Images.
+        this.shark01 = this.add.image(150, 500, "shark01");
+        this.shark01.scaleX = 0.3;
+        this.shark01.scaleY = 0.3;
 
     }
     update() {
