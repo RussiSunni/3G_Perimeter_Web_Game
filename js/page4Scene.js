46fd9ffc066e2a391ea6d148fc13abaa6c8b12d2
RussiSunni <jonathandyason@hotmail.com>
2022-10-16 17:57:24 +0200

    asd

--- js/page4Scene.js
+++ js/page4Scene.js
@@ -42,20 +42,27 @@ class Page4Scene extends Phaser.Scene {
         this.roundedRect1 = this.add.graphics();
         this.roundedRect1.fillStyle(0x70ad47, 1);
         this.roundedRect1.fillRoundedRect(300, game.config.height / 2 - 50, 800, 80, 16);
+        this.roundedRect1.setInteractive(new Phaser.Geom.Rectangle(300, game.config.height / 2 - 50, 800, 80), Phaser.Geom.Rectangle.Contains);
+        this.roundedRect1.on("pointerdown", this.correctAnswer, this);
 
         this.roundedRect2 = this.add.graphics();
         this.roundedRect2.fillStyle(0x70ad47, 1);
         this.roundedRect2.fillRoundedRect(300, game.config.height / 2 + 150 - 50, 800, 80, 16);
+        this.roundedRect1.setInteractive(new Phaser.Geom.Rectangle(300, game.config.height / 2 + 150 - 50, 800, 80), Phaser.Geom.Rectangle.Contains);
+        this.roundedRect1.on("pointerdown", this.incorrectAnswer, this);
 
         // Text.
         this.text2 = this.add.text(400, game.config.height - 50, "Click the correct answer.", { fontFamily: "Arial", fontSize: "40px" });
         this.text3 = this.add.text(310, game.config.height / 2 - 35, "Since this is a rectangle, we know that the parallel side lengths are equal \nin length. The bottom side length is 9 ft and the right side length is 12 ft.", { fontFamily: "Arial", fontSize: "22px" });
         this.text4 = this.add.text(310, game.config.height / 2 + 120, "If we add 9 + 12, we know that the bottom side length is 21 ft and the right \nside length is also 21 ft.", { fontFamily: "Arial", fontSize: "22px" });
-
     }
 
-    onButtonDown() {
+    correctAnswer() {
+        this.scene.start("Page5Scene");
+    }
 
+    incorrectAnswer() {
+        console.log("incorrect");
     }
 
 }
