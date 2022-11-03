722d9d77f84338e900f9c08e3443415860e26955
RussiSunni <jonathandyason@hotmail.com>
2022-11-03 15:37:53 +0200

    asd

--- js/page10Scene.js
+++ js/page10Scene.js
@@ -12,6 +12,11 @@ class Page10Scene extends Phaser.Scene {
         this.load.image("cross", "images/cross.png");
 
         this.load.audio("audio1", ["audio/page10/1-SubBlock_Oh-dear-Nitro-the-shark-is-ch.mp3"]);
+        this.load.audio("applause", ["audio/applause.wav"]);
+        this.load.audio("correctAudio", ["audio/page10/3-SubBlock_5-is-correct-Excellent-work.mp3"]);
+
+        this.load.audio("buzzer", "audio/buzzer.mp3");
+        this.load.audio("incorrectAudio", ["audio/page10/4-SubBlock_Oh-dear-no-That-is-incorrect.mp3"]);
     }
 
     create() {
@@ -22,7 +27,14 @@ class Page10Scene extends Phaser.Scene {
         this.background.on("pointerdown", this.onButtonDown, this);
 
         this.audio1Sound = this.sound.add("audio1");
-
+        this.audio1Sound.on("complete", this.playSharkAnimation, this);
+        this.applauseSound = this.sound.add("applause");
+        this.applauseSound.on("complete", this.playSecondCorrectSound, this);
+        this.correctSound = this.sound.add("correctAudio");
+        this.correctSound.on("complete", this.playFishAnimation, this);
+        this.buzzerSound = this.sound.add("buzzer");
+        this.buzzerSound.on("complete", this.playSecondIncorrectSound, this);
+        this.incorrectSound = this.sound.add("incorrectAudio");
 
         // Images.
         this.logo = this.add.image(1150, 80, "logo");
@@ -43,7 +55,8 @@ class Page10Scene extends Phaser.Scene {
             answer1Container.scale = 1;
         });
         answer1Container.on('pointerdown', function () {
-            this.scene.start("Page11Scene");
+            // this.scene.start("Page11Scene");
+            this.applauseSound.play();
         }, this);
 
 
@@ -63,6 +76,7 @@ class Page10Scene extends Phaser.Scene {
         });
         answer2Container.on('pointerdown', function () {
             answer2Cross.alpha = 1;
+            this.buzzerSound.play();
         }, this);
 
         var answer3Circle = this.add.circle(0, 0, 70, 0xed7d31);
@@ -80,6 +94,7 @@ class Page10Scene extends Phaser.Scene {
         });
         answer3Container.on('pointerdown', function () {
             answer3Cross.alpha = 1;
+            this.buzzerSound.play();
         }, this);
 
         var answer4Circle = this.add.circle(0, 0, 70, 0xed7d31);
@@ -97,31 +112,32 @@ class Page10Scene extends Phaser.Scene {
         });
         answer4Container.on('pointerdown', function () {
             answer4Cross.alpha = 1;
+            this.buzzerSound.play();
         }, this);
 
-        var rect = this.add.rectangle(300, 250, 500, 200, 0x4472c4);
-        rect.setStrokeStyle(6, 0xffffff);
-        var triangle = this.add.triangle(175, 450, 0, 0, 250, 0, 125, 200, 0x4472c4);
+
+
+        var triangle = this.add.triangle(-123, 195, 0, 0, 250, 0, 125, 200, 0x4472c4);
         triangle.setStrokeStyle(6, 0xffffff);
-        var line1 = this.add.line(305, 350, 0, 0, 500, 0, 0x4472c4);
-        line1.setLineWidth(4);
-        //var line2 = this.add.line(550, 250, 0, 0, 0, 200, 0x4472c4);
-        //line2.setLineWidth(4);
-        //var line3 = this.add.line(50, 250, 0, 0, 0, 200, 0x4472c4);
-        //line3.setLineWidth(4);
-        // var line4 = this.add.line(110, 450, 0, 0, 130, 200, 0x4472c4);
-        // line4.setLineWidth(4);
-        //var line5 = this.add.line(240, 450, 130, 0, 0, 200, 0x4472c4);
-        //line5.setLineWidth(4);
+        var rect = this.add.rectangle(0, 0, 500, 200, 0x4472c4);
+        // rect.setStrokeStyle(6, 0xffffff);
+        var line1 = this.add.line(2, -100, 0, 0, 500, 0, 0xffffff);
+        line1.setLineWidth(3);
+        var line2 = this.add.line(-244, 0, 0, 0, 0, 200, 0xffffff);
+        line2.setLineWidth(3);
+        var line3 = this.add.line(250, 0, 0, 0, 0, 200, 0xffffff);
+        line3.setLineWidth(3);
 
 
-        this.text3 = this.add.text(150, 220, "Perimeter = 44in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
-        this.text4 = this.add.text(250, 110, "13in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
-        this.text5 = this.add.text(560, 230, "6in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
-        this.text6 = this.add.text(0, 230, "6in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
-        this.text7 = this.add.text(55, 430, "7in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
-        this.text8 = this.add.text(250, 430, "7in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
-        this.text9 = this.add.text(420, 350, "?", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+        this.text3 = this.add.text(-150, -30, "Perimeter = 44in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+        this.text4 = this.add.text(-50, -140, "13in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+        this.text5 = this.add.text(260, -20, "6in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+        this.text6 = this.add.text(-310, -20, "6in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+        this.text7 = this.add.text(-245, 180, "7in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+        this.text8 = this.add.text(-50, 180, "7in", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+        this.text9 = this.add.text(120, 100, "?", { fontFamily: "Arial", fontSize: "36px", fontStyle: "bold" });
+
+        var shapeContainer = this.add.container(310, 250, [triangle, rect, line1, line2, line3, this.text3, this.text4, this.text5, this.text6, this.text7, this.text8, this.text9]);
 
         this.fish02 = this.add.image(900, 350, "fish02");
         this.fish02.flipX = true;
@@ -143,11 +159,59 @@ class Page10Scene extends Phaser.Scene {
         var timer = this.time.delayedCall(2000, this.audio1Play, null, this);
         //var timedEvent = this.time.delayedCall(2000, this.audio1Play, this);
 
-        var shark02 = this.add.image(1400, 150, "shark02");
+        var shark02 = this.add.image(1700, 150, "shark02");
         shark02.flipX = true;
         shark02.scaleX = 2;
         shark02.scaleY = 2;
 
+        // Animation
+        this.timeline2 = this.tweens.createTimeline();
+
+        this.timeline2.add({
+            targets: [shark02],
+            x: 1300,
+            ease: 'Power1',
+            duration: 2000
+        });
+
+
+        // Animation
+        this.timeline3 = this.tweens.createTimeline();
+
+        this.timeline3.add({
+            targets: [this.fish02],
+            scale: 1.5,
+            ease: 'Power1',
+            duration: 1000
+        });
+
+        this.timeline3.add({
+            targets: [this.fish02],
+            y: 450,
+            ease: 'Power1',
+            duration: 1000
+        });
+
+        this.timeline3.add({
+            targets: [this.fish02],
+            x: 450,
+            ease: 'Power1',
+            duration: 1000
+        });
+
+        this.timeline3.add({
+            targets: [this.fish02],
+            y: 250,
+            ease: 'Power1',
+            duration: 1000
+        });
+
+        this.timeline3.add({
+            targets: [this.fish02],
+            x: 200,
+            ease: 'Power1',
+            duration: 1000
+        });
 
     }
 
@@ -157,6 +221,23 @@ class Page10Scene extends Phaser.Scene {
 
     audio1Play(sound) {
         this.audio1Sound.play()
+    }
+
+    playSharkAnimation() {
+        console.log("test")
+        this.timeline2.play();
+    }
+
+    playSecondCorrectSound() {
+        this.correctSound.play();
+    }
+
+    playFishAnimation() {
+        this.timeline3.play();
+    }
+
+    playSecondIncorrectSound() {
+        this.incorrectSound.play();
     }
 
 }
